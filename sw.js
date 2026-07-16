493ca403ecec8843b1559c77871a110a42da388e
muramasa912-sudo <muramasa912@gmail.com>
2026-07-16 17:17:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "shogakusei-sansu-navi-ipad";
-const CACHE_NAME = `${CACHE_PREFIX}-v9`;
+const CACHE_NAME = `${CACHE_PREFIX}-v10`;
 const APP_SHELL = [
   "./",
   "index.html",
